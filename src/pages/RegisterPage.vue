4d55ed17d19476539dfb9cbab0b4d37ef0307db1
Ealten <1jonasmadsenfake1@gmail.com>
2024-11-11 19:36:07 +0300

    update: split social login into component

--- src/pages/RegisterPage.vue
+++ src/pages/RegisterPage.vue
@@ -4,7 +4,7 @@ import { useAuthStore } from '../stores/auth'
 import { z } from 'zod'
 import useValidation from '../services/useValidation'
 import axios, { type AxiosError } from 'axios'
-import SocialLoginButton from '@/components/atoms/SocialLoginButton.vue'
+import SocialLoginGroup from '@/components/molecules/SocialLoginGroup.vue'
 
 const authStore = useAuthStore()
 
@@ -161,26 +161,8 @@ const register = async () => {
               Create an account
             </button>
             <div class="border-t border-slate-200 mx-6"></div>
-            <SocialLoginButton
-              text="Continue with Google"
-              @on-login="() => authStore.startOAuthLogin()"
-            >
-              <svg
-                class="mr-2 -ml-1 w-4 h-4"
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fab"
-                data-icon="google"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 488 512"
-              >
-                <path
-                  fill="currentColor"
-                  d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
-                ></path>
-              </svg>
-            </SocialLoginButton>
+
+            <SocialLoginGroup :resonse-status="Number($route.query.status)"></SocialLoginGroup>
             <p class="text-sm font-light text-gray-500">
               Already have an account?
               <RouterLink to="/login" class="font-medium text-indigo-600 hover:underline"
